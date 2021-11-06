8a61f553202a20ae37391c3e9ad529cee67ff26e
Brad Hamill <brad@hamill.com>
2021-11-06 15:36:47 -0600

    bring all code and tests current

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
-import express, { Request, Response } from 'express';
+import mongoose from 'mongoose'
+import express, { Request, Response } from 'express'
 import {
   requireAuth,
   validateRequest,
   NotFoundError,
   OrderStatus,
   BadRequestError,
-} from '@sgtickets/common';
-import { body } from 'express-validator';
-import { Ticket } from '../models/ticket';
-import { Order } from '../models/order';
-import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
-import { natsWrapper } from '../nats-wrapper';
+} from '@sgtickets/common'
+import { body } from 'express-validator'
+import { Ticket } from '../models/ticket'
+import { Order } from '../models/order'
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
-const router = express.Router();
+const router = express.Router()
 
-const EXPIRATION_WINDOW_SECONDS = 1 * 60;
+const EXPIRATION_WINDOW_SECONDS = 1 * 60
 
 router.post(
   '/api/orders',
@@ -24,28 +24,28 @@ router.post(
     body('ticketId')
       .not()
       .isEmpty()
-      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .custom((input: string) => mongoose.isValidObjectId(input))
       .withMessage('TicketId must be provided'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { ticketId } = req.body;
+    const { ticketId } = req.body
 
     // Find the ticket the user is trying to order in the database
-    const ticket = await Ticket.findById(ticketId);
+    const ticket = await Ticket.findById(ticketId)
     if (!ticket) {
-      throw new NotFoundError();
+      throw new NotFoundError()
     }
 
     // Make sure that this ticket is not already reserved
-    const isReserved = await ticket.isReserved();
+    const isReserved = await ticket.isReserved()
     if (isReserved) {
-      throw new BadRequestError('Ticket is already reserved');
+      throw new BadRequestError('Ticket is already reserved')
     }
 
     // Calculate an expiration date for this order
-    const expiration = new Date();
-    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
 
     // Build the order and save it to the database
     const order = Order.build({
@@ -53,8 +53,8 @@ router.post(
       status: OrderStatus.Created,
       expiresAt: expiration,
       ticket,
-    });
-    await order.save();
+    })
+    await order.save()
 
     // Publish an event saying that an order was created
     new OrderCreatedPublisher(natsWrapper.client).publish({
@@ -67,10 +67,10 @@ router.post(
         id: ticket.id,
         price: ticket.price,
       },
-    });
+    })
 
-    res.status(201).send(order);
+    res.status(201).send(order)
   }
-);
+)
 
-export { router as newOrderRouter };
+export { router as newOrderRouter }
--- tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
-import { Message } from 'node-nats-streaming';
-import { OrderCancelledEvent } from '@sgtickets/common';
-import { natsWrapper } from '../../../nats-wrapper';
-import { OrderCancelledListener } from '../order-cancelled-listener';
-import { Ticket } from '../../../models/ticket';
+import mongoose from 'mongoose'
+import { Message } from 'node-nats-streaming'
+import { OrderCancelledEvent } from '@sgtickets/common'
+import { natsWrapper } from '../../../nats-wrapper'
+import { OrderCancelledListener } from '../order-cancelled-listener'
+import { Ticket } from '../../../models/ticket'
 
 const setup = async () => {
-  const listener = new OrderCancelledListener(natsWrapper.client);
+  const listener = new OrderCancelledListener(natsWrapper.client)
 
-  const orderId = mongoose.Types.ObjectId().toHexString();
+  const orderId = new mongoose.Types.ObjectId().toHexString()
   const ticket = Ticket.build({
     title: 'concert',
     price: 20,
     userId: 'asdf',
-  });
-  ticket.set({ orderId });
-  await ticket.save();
+  })
+  ticket.set({ orderId })
+  await ticket.save()
 
   const data: OrderCancelledEvent['data'] = {
     id: orderId,
@@ -23,23 +23,23 @@ const setup = async () => {
     ticket: {
       id: ticket.id,
     },
-  };
+  }
 
   // @ts-ignore
   const msg: Message = {
     ack: jest.fn(),
-  };
+  }
 
-  return { msg, data, ticket, orderId, listener };
-};
+  return { msg, data, ticket, orderId, listener }
+}
 
 it('updates the ticket, publishes an event, and acks the message', async () => {
-  const { msg, data, ticket, orderId, listener } = await setup();
+  const { msg, data, ticket, orderId, listener } = await setup()
 
-  await listener.onMessage(data, msg);
+  await listener.onMessage(data, msg)
 
-  const updatedTicket = await Ticket.findById(ticket.id);
-  expect(updatedTicket!.orderId).not.toBeDefined();
-  expect(msg.ack).toHaveBeenCalled();
-  expect(natsWrapper.client.publish).toHaveBeenCalled();
-});
+  const updatedTicket = await Ticket.findById(ticket.id)
+  expect(updatedTicket!.orderId).not.toBeDefined()
+  expect(msg.ack).toHaveBeenCalled()
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
